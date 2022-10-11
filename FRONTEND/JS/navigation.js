f7d2aa3dcd27a7da45df242809c1dcf3e85cdec7
Whitney de Oliveira Okafor <114311806+WhitneyOOkafor@users.noreply.github.com>
2022-10-11 17:43:03 +0200

    created a navigation file where the js code related to navigation around the app will be; changed the 2 buttons from button tags to divs; made the divs clickable and made them redirect the user to the home page and first sign up page

---
+++ FRONTEND/JS/navigation.js
@@ -0,0 +1,13 @@
+//& navigate to signup page
+function goToSignUp1(){
+    $("#signup-btn").click(function(){
+        window.location.href = '../signupLogin/signup-1.html';
+    })
+}
+
+//& navigate to login page
+function login(){
+    $("#signup-btn").click(function(){
+        window.location.href = '../signupLogin/home.html';
+    })
+}
