c4cada04c3ade9cd2e57474148263e0d7d12f030
Whitney de Oliveira Okafor <114311806+WhitneyOOkafor@users.noreply.github.com>
2022-10-11 12:00:55 +0200

    created the splashcreen page and added the functionality that makes splashcreen pulse and then redirect to login/signup (index.html) after a period of time

---
+++ FRONTEND/JS/splashscreen.js
@@ -0,0 +1,5 @@
+// & this code makes the logo pulse andf then redirects the user to the login/signup
+//& after an interval of 3 seconds
+$(window).ready(window.setTimeout(function() {
+    window.location.href = '../signupLogin/index.html';
+}, 3000))
